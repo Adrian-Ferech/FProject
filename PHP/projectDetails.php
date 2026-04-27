d331070fbbaeb2a6280b0e9285c74fe06daed238
Adrian-Ferech <adrian18son@gmail.com>
2026-04-27 23:19:45 +0100

    Update projectDetails.php

--- PHP/projectDetails.php
+++ PHP/projectDetails.php
@@ -7,8 +7,18 @@ require_once 'db.php';
 
 if (isset($_GET['id'])) {
     $id = intval($_GET['id']);
-    // If project ID is found then get details for project
-    $stmt = $conn->prepare("SELECT title, description FROM projects WHERE Project_ID = ?");
+    // If there is a project ID found then display details
+    $stmt = $conn->prepare("
+    SELECT 
+        p.title, 
+        p.description, 
+        p.prerequisite, 
+        p.capacity,
+        l.Name AS lecturer_name
+    FROM projects p
+    JOIN lecturers l ON p.Lecturer_ID = l.Lecturer_ID
+    WHERE p.Project_ID = ?
+    ");
     $stmt->bind_param("i", $id);
     $stmt->execute();
     $result = $stmt->get_result();
@@ -28,12 +38,24 @@ if (isset($_GET['id'])) {
 <!DOCTYPE html>
 <html>
 <head>
+    <link rel="stylesheet" href="../CSS/stylesheet2.css">
     <title><?php echo htmlspecialchars($project['title']); ?></title>
 </head>
 <body>
-
+<!-- Display project details -->
 <h1><?php echo htmlspecialchars($project['title']); ?></h1>
-<p><?php echo htmlspecialchars($project['description']); ?></p>
+
+<p><strong>Description:</strong><br>
+<?php echo htmlspecialchars($project['description']); ?></p>
+
+<p><strong>Lecturer:</strong>
+<?php echo htmlspecialchars($project['lecturer_name']); ?></p>
+
+<p><strong>Prerequisite:</strong>
+<?php echo htmlspecialchars($project['prerequisite']); ?></p>
+
+<p><strong>Capacity:</strong>
+<?php echo htmlspecialchars($project['capacity']); ?></p>
 
 <form action="addPreference.php" method="post">
     <input type="hidden" name="project_id" value="<?php echo $id; ?>">
